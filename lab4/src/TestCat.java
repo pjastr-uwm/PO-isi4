a359c9a7e784d8b1249b145fcad14b3dc00090bd
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-10-25 14:14:23 +0200

    11

---
+++ lab4/src/TestCat.java
@@ -0,0 +1,25 @@
+public class TestCat {
+
+    public static void main(String[] args) {
+        Cat c1 = new Cat();
+        Cat c2 = c1.createCat("Azorek");
+        System.out.println(c2.name);
+        Cat c3 = c1.createCat(null);
+        System.out.println(c3);
+        System.out.println(c2);
+    }
+}
+
+class Cat{
+    String name;
+
+    Cat createCat(String value){
+        Cat temp = new Cat();
+        if (value == null)
+        {
+            return null;
+        }
+        temp.name = value;
+        return temp;
+    }
+}
