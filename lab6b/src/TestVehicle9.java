b099ae5c736ef36f7d773395a4dfd96649ad909e
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-11-22 14:06:02 +0100

    zad9

---
+++ lab6b/src/TestVehicle9.java
@@ -0,0 +1,9 @@
+import pl.edu.uwm.zad9.Car;
+import pl.edu.uwm.zad9.Vehicle;
+
+public class TestVehicle9 {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle(" sss","dfd");
+        Car c1 = new Car("ryt","tytr",5);
+    }
+}
---
+++ lab6b/src/pl/edu/uwm/zad9/Vehicle.java
@@ -0,0 +1,12 @@
+package pl.edu.uwm.zad9;
+
+public class Vehicle {
+
+    private String brand;
+    private String model;
+
+    public Vehicle(String brand, String model){
+        this.brand = brand;
+        this.model = model;
+    }
+}
