8c338b285d334475b1e965be392e13ebda4050a4
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-12-13 13:14:56 +0100

    zad10

---
+++ lab9/src/rekordy/zad10/TestBankAccount.java
@@ -0,0 +1,11 @@
+package rekordy.zad10;
+
+public class TestBankAccount {
+
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount("564553");
+        BankAccount b2 = new BankAccount("7567", 453646.3);
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
